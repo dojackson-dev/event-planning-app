327bc5f0971c023ec89d0fdb655a9fb3a3b4d686
Larry Nixon <contact@dojackson.com>
2026-08-31 22:10:42 -0400

    chore(mobile): bump iOS build number to 14 for TestFlight submission

--- packages/mobile/app.config.js
+++ packages/mobile/app.config.js
@@ -12,7 +12,7 @@ module.exports = {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.eventplanning.app",
-      buildNumber: "13",
+      buildNumber: "14",
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false
       }
